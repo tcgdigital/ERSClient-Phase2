530dc2a342ed522a41b71005e9d67890e9767252
Sandip Ghosh <sandip.ghosh@tcg-digital.com>
2017-03-08 16:47:40 +0530

    Modified in routing

--- src/app/pages/dashboard/dashboard.routing.ts
+++ src/app/pages/dashboard/dashboard.routing.ts
@@ -7,8 +7,8 @@ const dashboardRoutes: Routes = [
         path: '',
         component: DashboardComponent,
         children: [
-            { path: 'affectedPeople', loadChildren: './affected.people/affected.people.module#AffectedPeopleModule' },
-            { path: 'affectedObjects', loadChildren: './affected.objects/affected.objects.module#AffectedObjectsModule' },
+            { path: 'people', loadChildren: './affected.people/affected.people.module#AffectedPeopleModule' },
+            { path: 'cargo', loadChildren: './affected.objects/affected.objects.module#AffectedObjectsModule' },
             { path: 'callCentre', loadChildren: './call.centre/call.centre.module#CallCentreModule' },
             { path: 'actionable', loadChildren: './actionable/actionable.module#ActionableModule' }           
         ]
