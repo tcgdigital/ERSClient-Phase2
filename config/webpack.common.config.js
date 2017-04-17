f8024206371c1ed1bfd2c46d1887a653e2c68286
Sandip Ghosh <sandip.ghosh@tcg-digital.com>
2017-04-17 16:40:26 +0530

    Some asthatic changes

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -242,8 +242,8 @@ module.exports = function (options) {
              */
             new ContextReplacementPlugin(
                 // The (\\|\/) piece accounts for path separators in *nix and Windows
-                /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
-                // /angular(\\|\/)core(\\|\/)@angular/,
+                // /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+                /angular(\\|\/)core(\\|\/)@angular/,
                 helpers.root('src') // location of your src
             ),
 
