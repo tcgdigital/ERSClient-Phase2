a69839d31b4ef9d05b80120790ca33be820ddaf2
Sandip Ghosh <sandip.ghosh@tcg-digital.com>
2017-06-07 16:04:16 +0530

    Implementing time-zone detector

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -31,6 +31,7 @@ import 'lodash';
 import 'air-datepicker';
 import 'moment';
 import 'underscore';
+import 'jstimezonedetect';
 import '../node_modules/air-datepicker/dist/js/i18n/datepicker.en';
 import '../node_modules/sweet-dropdown/dist/min/jquery.sweet-dropdown.min';
 import 'highcharts';
