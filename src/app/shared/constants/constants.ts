101ffb74a5abd28a1a5c983be1c9065712c62df9
sanga987 <sanga.chatterjee@tcg-digital.com>
2017-06-08 20:56:54 +0530

    dashboard changes

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -46,7 +46,7 @@ export interface IActionableStatus {
 
 export class GlobalConstants {
     // public static EXTERNAL_URL: string = 'http://202.54.73.219:86/';
-    public static EXTERNAL_URL: string = 'http://localhost:5001/';
+    public static EXTERNAL_URL: string = 'http://172.20.23.110:84/';
     public static CLIENT_ID: string = 'A924D89F487E4F888EA8CFDB1AE4E9D3';
     public static GRANT_TYPE: string = 'password';
     public static ODATA: string = 'odata';
