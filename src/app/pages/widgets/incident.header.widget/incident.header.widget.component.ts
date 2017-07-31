ad563c7b9d225cffa2d895cf0a94cff1fe674c73
Shayan Sarkar <ShayanS@skytechsolutions.com>
2017-07-31 19:31:25 +0530

    userprofile and isdrill

--- src/app/pages/widgets/incident.header.widget/incident.header.widget.component.ts
+++ src/app/pages/widgets/incident.header.widget/incident.header.widget.component.ts
@@ -4,6 +4,7 @@ import {
     OnChanges, SimpleChange
 } from '@angular/core';
 import { KeyValue, UtilityService, GlobalStateService } from '../../../shared';
+import { IncidentModel, IncidentService } from '../../incident/components';
 
 @Component({
     selector: 'incident-header-widget',
@@ -24,14 +25,26 @@ export class IncidentHeaderWidgetComponent implements OnInit, OnChanges {
     public useLink: boolean;
     //public isShowViewReadonlyCrisis: boolean = true;
     public currentDepartmentId: number = 0;
+    public IsDrillIndicator: boolean = false;
+    private incidentId: number;
 
-    constructor(private globalState: GlobalStateService) { }
+    constructor(private globalState: GlobalStateService,
+        private incidentService: IncidentService) { }
 
     ngOnInit() {
 
         this.useLink = true;
         this.globalState.Subscribe('departmentChange', (model: KeyValue) => this.departmentChangeHandler(model));
         this.currentDepartmentId = +UtilityService.GetFromSession('CurrentDepartmentId');
+        this.incidentId = +UtilityService.GetFromSession('CurrentIncidentId');
+        this.getIncident(this.currentIncident.Value);
+    }
+
+    getIncident(incidentId: number): void {
+        this.incidentService.GetIncidentById(incidentId)
+            .subscribe((item: IncidentModel) => {
+                this.IsDrillIndicator = item.IsDrill;
+            });
     }
 
     // public onViewIncidentClick(): void {
@@ -41,6 +54,7 @@ export class IncidentHeaderWidgetComponent implements OnInit, OnChanges {
     public ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
         if (this.currentIncident !== undefined) {
             this.incidentName = this.currentIncident.Key;
+            this.getIncident(this.currentIncident.Value);
         }
         if (this.currentDepartment !== undefined) {
             this.departmentName = this.currentDepartment.Key;
