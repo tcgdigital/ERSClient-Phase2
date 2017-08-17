d3e3dff5c339b22e2b33b52a7da77db3c5493701
Sandip Ghosh <sandipg@skytechsolutions.intra>
2017-08-18 00:01:31 +0530

    Fix the arrow scroll position of responsive table inside bsModal window

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -62,10 +62,10 @@ export enum StorageType {
 export class GlobalConstants {
     // public static EXTERNAL_URL: string = 'http://202.54.73.219/';
     // public static EXTERNAL_URL: string = 'http://172.20.23.110:99/';
-    //  public statkic EXTERNAL_URL: string = 'http://localhost:5001/';
+     public static EXTERNAL_URL: string = 'http://localhost:5001/';
     //public static EXTERNAL_URL: string = 'http://172.20.23.110:84/';
 
-    public static EXTERNAL_URL: string = 'http://cmgtest.pal.com.ph:8080/';
+    // public static EXTERNAL_URL: string = 'http://cmgtest.pal.com.ph:8080/';
 
     public static NOTIFICATION_URL: string = `${GlobalConstants.EXTERNAL_URL}Notification/Hubs`;
     public static CLIENT_ID: string = 'A924D89F487E4F888EA8CFDB1AE4E9D3';
