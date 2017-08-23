4e8ca913392d8df4b365f1087a0c7e16e6d9a636
Surajit Sen <surajits@skytechsolutions.intra>
2017-08-23 16:34:41 +0530

    changes

--- src/app/pages/shared.components/demand/components/demand.entry.component.ts
+++ src/app/pages/shared.components/demand/components/demand.entry.component.ts
@@ -636,7 +636,8 @@ export class DemandEntryComponent implements OnInit, OnDestroy {
                                 }
                                 else {
                                     this.toastrService.success('Demand successfully updated.', 'Success', this.toastrConfig);
-                                    this.dataExchange.Publish('DemandAddedUpdated', this.demandModelEdit.DemandId);
+                                    const num = UtilityService.UUID();
+                                    this.globalStateProxyOpen.NotifyDataChanged('DemandAddedUpdated', num);
                                     this.initializeForm();
                                     this.demandModel = new DemandModel();
                                     this.showAdd = false;
@@ -646,6 +647,8 @@ export class DemandEntryComponent implements OnInit, OnDestroy {
                                 }
                             });
                     }
+
+                    this.filesToUpload.length = null;
                 });
         }
     }
