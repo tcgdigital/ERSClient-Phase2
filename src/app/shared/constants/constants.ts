2ee000b6f3c7f75f8f1839efb2dbe43e72171780
Shayan Sarkar <ShayanS@skytechsolutions.com>
2017-09-11 18:20:58 +0530

    member engagement active users show only

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -329,9 +329,9 @@ export class GlobalConstants {
         { value: 'NonFlight', key: 2 }
     ];
 
-    public static TargetDepartmentTravel: number = 36; // 1138; 36;10
-    public static TargetDepartmentAdmin: number = 36; //1138;36; 13
-    public static TargetDepartmentCrew: number = 36; //1138;36; 9
+    public static TargetDepartmentTravel: number = 10; // 1138; 36;10
+    public static TargetDepartmentAdmin: number = 13; //1138;36; 13
+    public static TargetDepartmentCrew: number = 9; //1138;36; 9
     public static DemandTypeId: number = 1;
     public static RequiredLocation: string = 'Office';
     public static ScheduleTimeForCallback: number = 75;
