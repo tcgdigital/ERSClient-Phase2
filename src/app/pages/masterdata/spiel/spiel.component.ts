f8aeaefdd86397c83522657c232459975ba8ecaa
nagsoumittcg <soumit.nag@tcg-digital.com>
2017-10-12 20:00:46 +0530

    bug resolved for the configuration in the master data.

--- src/app/pages/masterdata/spiel/spiel.component.ts
+++ src/app/pages/masterdata/spiel/spiel.component.ts
@@ -5,11 +5,12 @@ import {
     ReactiveFormsModule
 } from '@angular/forms';
 import { KeyValueService, KeyValueModel, ResponseModel } from '../../../shared';
+import { DepartmentService, DepartmentModel } from '../department';
 
 @Component({
     selector: 'dept-main',
     encapsulation: ViewEncapsulation.None,
-    providers: [KeyValueService],
+    providers: [KeyValueService, DepartmentService],
     templateUrl: './views/spiel.entry.view.html',
     styleUrls: ['./styles/spiel.style.scss']
 })
@@ -18,17 +19,34 @@ export class SpileComponent implements OnInit {
     public generalform: FormGroup;
     public submitted: boolean = false;
     public activeKeyValues: KeyValueModel[] = [];
+    public activeDepartments: DepartmentModel[] = [];
+    public isTextbox: boolean = false;
+    public isTextArea: boolean = false;
+    public isDropdown: boolean = false;
     private updatedKeyValue: KeyValueModel;
 
     constructor(private keyValueService: KeyValueService,
         private toastrService: ToastrService,
         private toastrConfig: ToastrConfig,
-    ) { }
+        private departmentService: DepartmentService) { }
 
     public ngOnInit() {
-        this.updatedKeyValue=new KeyValueModel();
+        this.isTextbox = false;
+        this.isTextArea = false;
+        this.isDropdown = false;
+        this.updatedKeyValue = new KeyValueModel();
         this.getAllActiveKeyValues();
+        this.getAllActiveDepartments();
         this.initializeForm();
+    }
+
+    getAllActiveDepartments(): void {
+        this.departmentService.GetAllActiveDepartments()
+            .subscribe((departments: ResponseModel<DepartmentModel>) => {
+                if (departments.Count > 0) {
+                    this.activeDepartments = departments.Records;
+                }
+            });
     }
 
     getAllActiveKeyValues(): void {
@@ -49,13 +67,31 @@ export class SpileComponent implements OnInit {
             SpielType: new FormControl('', [Validators.required]),
             SpielText: new FormControl('', [Validators.required])
         });
-
+        this.isTextbox = false;
+        this.isTextArea = false;
+        this.isDropdown = false;
     }
 
-    
 
-    keyvalueChange(Key: string, activeKeyValues: KeyValueModel[]): void {
+
+    keyvalueChange(target: any, Key: string, activeKeyValues: KeyValueModel[]): void {
         if (Key != '') {
+            let controlType: string = target.selectedOptions[0].getAttribute('ControlType');
+            if (controlType.toLowerCase() == 'textbox') {
+                this.isTextbox = true;
+                this.isDropdown = false;
+                this.isTextArea = false;
+            }
+            else if (controlType.toLowerCase() == 'textarea') {
+                this.isTextbox = false;
+                this.isDropdown = false;
+                this.isTextArea = true;
+            }
+            else if (controlType.toLowerCase() == 'dropdown') {
+                this.isTextbox = false;
+                this.isDropdown = true;
+                this.isTextArea = false;
+            }
             const activeOrganization: KeyValueModel = activeKeyValues
                 .find((x: KeyValueModel) => x.Key === Key);
 
@@ -63,28 +99,32 @@ export class SpileComponent implements OnInit {
         }
         else {
             this.generalform.controls["SpielText"].setValue('');
+            this.isTextbox = false;
+            this.isDropdown = false;
+            this.isTextArea = false;
         }
 
     }
 
-   
+
 
     public spielSave(): void {
         if (this.generalform.valid) {
             let KeyVal: KeyValueModel = null;
-            
+
             const activeOrganization: KeyValueModel = this.activeKeyValues
-            .find((x: KeyValueModel) => x.Key === this.generalform.controls["SpielType"].value);
-            
+                .find((x: KeyValueModel) => x.Key === this.generalform.controls["SpielType"].value);
+
             this.updatedKeyValue.Value = this.generalform.controls["SpielText"].value;
             KeyVal = this.updatedKeyValue;
-            KeyVal.KeyValueId=activeOrganization.KeyValueId;
+            KeyVal.KeyValueId = activeOrganization.KeyValueId;
             if (KeyVal != null) {
                 delete KeyVal.ActiveFlag;
                 delete KeyVal.CreatedBy;
                 delete KeyVal.CreatedOn;
                 delete KeyVal.Description;
                 delete KeyVal.Key;
+                delete KeyVal.ControlType;
                 this.keyValueService.Update(KeyVal, KeyVal.KeyValueId).subscribe(() => {
                     this.initializeForm();
                     this.getAllActiveKeyValues();
--- src/app/shared/models/keyvalue.model.ts
+++ src/app/shared/models/keyvalue.model.ts
@@ -2,16 +2,18 @@ import { BaseModel } from './base.model';
 
 export class KeyValueModel extends BaseModel {
 
-        public KeyValueId: number;
-        public Key: string;
-        public Value: string;
-        public Description: string;
+	public KeyValueId: number;
+	public Key: string;
+	public Value: string;
+	public Description: string;
+	public ControlType: string;
 
-        constructor() {
-                super();
-                this.KeyValueId = 1;
-                this.Key = '';
-                this.Value = '';
-                this.Description = '';
-        }
+	constructor() {
+		super();
+		this.KeyValueId = 1;
+		this.Key = '';
+		this.Value = '';
+		this.Description = '';
+		this.ControlType = '';
+	}
 }
