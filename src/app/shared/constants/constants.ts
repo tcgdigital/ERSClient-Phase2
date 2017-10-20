b3aa90eacac73e0eb0af44f878f9695fc159cf40
Sandip Ghosh <sandipg@skytechsolutions.intra>
2017-10-20 14:26:32 +0530

    Merge conflict changes'

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -60,15 +60,9 @@ export enum StorageType {
 }
 
 export class GlobalConstants {
-<<<<<<< HEAD
     // public static EXTERNAL_URL: string = 'http://api.cmgprod.pal.com.ph/';
     public static EXTERNAL_URL: string = 'http://localhost:5001/';
     // public static EXTERNAL_URL: string = 'http://172.20.23.110:99/';
-=======
-    //public static EXTERNAL_URL: string = 'http://api.cmgprod.pal.com.ph/';
-    //public static EXTERNAL_URL: string = 'http://localhost:5001/';
-    public static EXTERNAL_URL: string = 'http://172.20.23.110:99/';
->>>>>>> master
 
     public static NOTIFICATION_URL: string = `${GlobalConstants.EXTERNAL_URL}Notification/Hubs`;
     public static CLIENT_ID: string = 'A924D89F487E4F888EA8CFDB1AE4E9D3';
